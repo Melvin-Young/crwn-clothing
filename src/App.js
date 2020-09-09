39fcbc1229986daef77bc59825c5bdf54d800823
Melvin Young <melviny6@gmail.com>
2020-09-09 16:51:03 -0500

    menu items use withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import HomePage from './pages/hompage/hompage.component';
+import { Switch, Route } from 'react-router-dom';
 
+const HatsPage = () => (
+	<div>
+		<h1>Hats Page</h1>
+	</div>
+);
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello world</h1>
+		<div>
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/hats" component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
