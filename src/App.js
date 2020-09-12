bdde1ded7589298f44a721adcbca8bfd1984781f
Melvin Young <melviny6@gmail.com>
2020-09-12 14:31:41 -0500

    add user sign up, and firebase utils including ability to store authenticated user to db

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/hompage/hompage.component';
 import ShopPage from '../src/pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
 	state = {
@@ -17,9 +17,21 @@ class App extends Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged((currentUser) =>
-			this.setState({ currentUser })
-		);
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+				userRef.onSnapshot((snapShot) => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data(),
+						},
+					});
+					console.log(this.state);
+				});
+			}
+			this.setState({ currentUser: userAuth });
+		});
 	}
 
 	componentWillUnmount() {
